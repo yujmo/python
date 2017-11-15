a4563575cd604768c6057ade1cb70fb91dedbd6f
yujmo <yujmo94@gmail.com>
2017-11-15 20:38:12 +0800

    add some files

--- mo_soft_new_technology.py
+++ mo_soft_new_technology.py
@@ -8,6 +8,7 @@ import warnings
     开发环境：
         1. python3.6
         2. mysql.connector连接MariaDB数据库
+        3. Linux，Windows没有测试过
     环境配置：
         1. MySQL数据库 port:13306
         2. 安装lib， pip install pymysql
@@ -33,7 +34,7 @@ class InitTable:
         exec_Mysql(sql)
 
 def exec_Mysql(sql,search=0):
-    mysql_connection = pymysql.connect(host="openfaas.cn",port=13306,user="root",passwd="*******************",database="test")
+    mysql_connection = pymysql.connect(host="**********",port=13306,user="***************",passwd="***************",database="test")
     cursor = mysql_connection.cursor()
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
@@ -53,30 +54,43 @@ def add_Data():
     if input("Please ensure your input(y/Y)") in "yY":
         sql = "INSERT INTO test.nis(name,age,tel) VALUES(\""+name+"\", "+age+", \""+tel +"\");"
         exec_Mysql(sql)
+
 def del_Data():
     name = input("Please input the name:")
     sql = "DELETE FROM nis where name=\"" + name + "\""
     exec_Mysql(sql)
 
 def alter_Data():
-    name = input("Please input the name:")
+    search_Data()
+    name = input("Please input the name that need alter:")
+    age = input("Please input the age:")
+    tel = input("Please input the tel:")
+    sql = "UPDATE nis SET "
+    if age:
+        sql = sql + "age=" + "\"" + age + "\""
+        if tel:    sql = sql + ","
+    if tel:    sql = sql + "tel=" + "\"" + tel + "\""
+    sql = sql + " where name=" + "\"" + name + "\""
+    exec_Mysql(sql)
+    search_Data(name)
 
-def search_Data():
-    sql = "select " + " * " +" from nis"
+def search_Data(name=0):
+    sql = "select * from nis"
+    if name:    sql = sql +" where name=" + "\"" + name + "\""
     datas = [x for x in exec_Mysql(sql,1)]
-    json_datas = json.dumps(datas,indent=1)
-    print(json_datas)
+    print(json.dumps(datas,indent=1))
 
 if __name__ == "__main__":
     InitTable()
     operator = {"add":add_Data,"del":del_Data,"alter":alter_Data,"search":search_Data}
     strings = """
-    说明：本程序实现了对员工信息的增、删、改、查
+    说明：本程序实现了对员工信息的增、删、改、查，如果需要查看数据，输入search即可。
         增      删      改        查
         add     del     alter     search
     """
-    print(strings)
-    try:
-        operator[input("What is your exec:").strip()]()
-    except KeyError:
-        print("Error: Unkown the input command")
+    while input("Enter q to exit or enter any key to continue:") != "q":
+        print(strings)
+        try:
+            operator[input("What is your exec:").strip()]()
+        except KeyError:
+            print("Error: Unkown the input command")
